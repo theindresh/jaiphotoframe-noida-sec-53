906ebae9746a9455fbe8656e28414be4772a119b
INDRESH BHARATI <indreshbharati77@gmail.com>
2026-09-20 18:44:57 +0530

    Switch to BrowserRouter with basename and SPA 404 handler for clean URLs without hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { FrameProvider } from './context/FrameContext'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
@@ -12,7 +12,7 @@ import './App.css'
 export default function App() {
   return (
     <FrameProvider>
-      <HashRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
@@ -24,7 +24,7 @@ export default function App() {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </FrameProvider>
   )
 }
